f76ce1423499b02870b43f78e036cfb6789b19e2
Preethi <selvampreethi360@gmail.com>
2023-09-13 08:52:00 +0530

    add nested elements

--- Episode01/app.js
+++ Episode01/app.js
@@ -1,15 +1,41 @@
-//create an h1 React Element
-const h1 = React.createElement(
-  "h1",
-  { id: "heading" }, //attributes to that tag
-  "Hello World from React!" //children
-);
+/*
 
-console.log(h1); //It's an object
+<div id="parent">
+  <div id="child1">
+    <h1>I'm h1 tag</h1>
+    <h2>I'm h2 tag</h2>
+  </div>
+  <div id="child2">
+    <h1>I'm h1 tag</h1>
+    <h2>I'm h2 tag</h2>
+  </div>
+</div>
+
+*/
+
+//create an h1 React Element
+// const h1 = React.createElement(
+//   "h1",
+//   { id: "heading" }, //attributes to that tag
+//   "Hello World from React!" //children
+// );
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+
+console.log(parent); //It's an object
 
 //create a root inside React - create a root element on <div id="root"></div>
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// Render h1 inside root in 2 steps
-// 1. Convert object to H1 element
+// Render parent inside root in 2 steps
+// 1. Convert object to parent element
 // 2. Put it on DOM
-root.render(h1);
+root.render(parent);
