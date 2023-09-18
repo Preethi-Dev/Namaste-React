7f4872601a138bb2fa79091d7aecdc804311bf19
Preethi <selvampreethi360@gmail.com>
2023-09-18 11:24:46 +0530

    import React and ReactDOM from node_modules

--- Episode02/app.js
+++ Episode02/app.js
@@ -20,6 +20,9 @@
 //   "Hello World from React!" //children
 // );
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
