9889f08c3601d344bdf3aceccac876add6cb88e1
Preethi <selvampreethi360@gmail.com>
2023-10-12 16:32:37 +0530

    add body component

--- Episode04/app.js
+++ Episode04/app.js
@@ -37,10 +37,41 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = (resImg, resName, resRating) => {
+  return (
+    <div className="res-card">
+      <img width="100%" src={resImg} />
+      <h3 style={{ textTransform: "uppercase" }}>{resName}</h3>
+      <p>{resRating}</p>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {RestaurantCard(
+          "https://cdn-p300.americantowns.com/img/article/nj-indian-restaurants-1.jpg",
+          "Meghana foods",
+          "4.4 Ratings"
+        )}
+        {RestaurantCard(
+          "https://www.eatthis.com/wp-content/uploads/sites/4/2021/11/kfc-fried-chicken.jpg?quality=82&strip=1",
+          "KFC",
+          "4.8 Ratings"
+        )}
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
