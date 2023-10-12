b11731228555d97da4ab450efbcbc86e1304514a
Preethi <selvampreethi360@gmail.com>
2023-10-12 15:35:54 +0530

    create and add Header component

--- Episode04/app.js
+++ Episode04/app.js
@@ -16,10 +16,31 @@ import ReactDOM from "react-dom/client";
  *   - Contact
  */
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.creativefabrica.com/wp-content/uploads/2022/03/07/Restaurant-yummy-food-logo-design-Graphics-26620420-1-580x387.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
-    <div>
-      <h1>Food Ordering App</h1>
+    <div className="app">
+      <Header />
     </div>
   );
 };
