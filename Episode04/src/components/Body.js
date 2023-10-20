0f66809a1150608ad185847366986a722055a103
Preethi <selvampreethi360@gmail.com>
2023-10-20 08:47:48 +0530

    add Top Rated Restraunt btn

--- Episode04/src/components/Body.js
+++ Episode04/src/components/Body.js
@@ -5,7 +5,9 @@ import { v4 as uuidv4 } from "uuid";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn">Top Rated Restraunt</button>
+      </div>
       <div className="res-container">
         {restraunts.map((restraunt) => (
           <RestaurantCard key={uuidv4()} resData={restraunt} />
