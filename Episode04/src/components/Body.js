7e1b3063eadfb82ce6db332e18e95e4f117f3f83
Preethi <selvampreethi360@gmail.com>
2023-10-20 16:40:32 +0530

    create and add state variable filters top rate restraunt and clears the filter

--- Episode04/src/components/Body.js
+++ Episode04/src/components/Body.js
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import { restraunts } from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 import { v4 as uuidv4 } from "uuid";
 
 const Body = () => {
+  //local state variable
+  let [listOfRestraunts, setListOfRestraunts] = useState(restraunts);
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn">Top Rated Restraunt</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let topRated = listOfRestraunts.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestraunts(topRated);
+          }}
+        >
+          Top Rated Restraunt
+        </button>
+      </div>
+      <div className="clear">
+        <button
+          className="clear-btn"
+          onClick={() => {
+            setListOfRestraunts(restraunts);
+          }}
+        >
+          clear
+        </button>
       </div>
       <div className="res-container">
-        {restraunts.map((restraunt) => (
+        {listOfRestraunts.map((restraunt) => (
           <RestaurantCard key={uuidv4()} resData={restraunt} />
         ))}
       </div>
