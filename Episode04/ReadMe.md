6d40f413d719bcab0a4aa17ebbbd4724e6afb685
Preethi <selvampreethi360@gmail.com>
2023-10-27 11:53:12 +0530

    add toggle login and logout btn

---
+++ Episode04/ReadMe.md
@@ -0,0 +1,14 @@
+## Namaste Food App Component Design
+
+- Header
+  - Logo
+  - Nav Items
+- Body
+  - Search
+  - RestrauntContainer
+  - RestrauntCard
+- Footer
+  - Copyright
+  - Links
+  - Address
+  - Contact
--- Episode04/src/components/Header.js
+++ Episode04/src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_LINK } from "../utils/constants";
 
 const Header = () => {
+  let [log, setLog] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              log === "Login" ? setLog("Logout") : setLog("Login");
+            }}
+          >
+            {log}
+          </button>
         </ul>
       </div>
     </div>
