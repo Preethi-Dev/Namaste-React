e1cc3255b5b2ba56e985ba1351e8d0bd3730f6da
Preethi <selvampreethi360@gmail.com>
2023-11-14 13:41:00 +0530

    read value from url and fetch the data

--- Episode04/src/components/RestaurantMenu.js
+++ Episode04/src/components/RestaurantMenu.js
@@ -1,17 +1,15 @@
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
+  const { resId } = useParams();
+
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9744943&lng=80.2105601&restaurantId=38897&catalog_qa=undefined&submitAction=ENTER"
-    );
-
+    const data = await fetch(MENU_API + resId);
     const json = await data.json();
-
-    console.log(json.data);
-
     setResInfo(json.data);
   };
 
@@ -20,6 +18,8 @@ const RestaurantMenu = () => {
   }, []);
 
   if (resInfo === null) return <Shimmer />;
+
+  console.log(resInfo);
 
   const { name, totalRatingsString } = resInfo?.cards[0]?.card?.card?.info;
 
--- Episode04/src/utils/constants.js
+++ Episode04/src/utils/constants.js
@@ -3,3 +3,6 @@ export const CDN_LINK =
 
 export const LOGO_LINK =
   "https://www.creativefabrica.com/wp-content/uploads/2022/03/07/Restaurant-yummy-food-logo-design-Graphics-26620420-1-580x387.jpg";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9744943&lng=80.2105601&restaurantId=";
