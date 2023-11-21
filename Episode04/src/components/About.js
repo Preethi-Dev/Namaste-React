494680b01ccb1bf1344f1fd81bfe335146d24c6f
Preethi <selvampreethi360@gmail.com>
2023-11-21 08:33:57 +0530

    render functional and class-based components

--- Episode04/src/components/About.js
+++ Episode04/src/components/About.js
@@ -1,7 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
       <h1>About Us</h1>
+      <User name={"Preethi"} location={"TamilNadu"} />
+      <UserClass name={"Preethi (class)"} location={"TamilNadu"} />
     </div>
   );
 };
