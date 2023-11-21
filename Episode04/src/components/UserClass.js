0a814a6449af4c681711584d4795a625e25577bd
Preethi <selvampreethi360@gmail.com>
2023-11-21 08:32:42 +0530

    create class based component

---
+++ Episode04/src/components/UserClass.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: {this.props.name}</h2>
+        <h3>Location: {this.props.location}</h3>
+        <h3>Conatct: @PREETHI</h3>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
