23247ff67e53e7b16c8466e7aa7aad39ab2c6eec
Preethi <selvampreethi360@gmail.com>
2023-12-04 11:38:04 +0530

    convert functional to class component - About component

--- Episode04/src/components/About.js
+++ Episode04/src/components/About.js
@@ -1,14 +1,27 @@
+import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <User name={"Preethi"} location={"TamilNadu"} />
-      <UserClass name={"Preethi (class)"} location={"TamilNadu"} />
-    </div>
-  );
-};
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constructor");
+  }
+
+  componentDidMount() {
+    console.log("parent component did mount");
+  }
+
+  render() {
+    console.log("parent render");
+    return (
+      <div>
+        <h1>About Us</h1>
+
+        <UserClass name={"First (class)"} location={"TamilNadu"} />
+      </div>
+    );
+  }
+}
 
 export default About;
