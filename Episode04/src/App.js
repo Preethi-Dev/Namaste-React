05b69b71c83f2c1735cd4244a03a292d9133457c
Preethi <selvampreethi360@gmail.com>
2023-12-04 11:42:07 +0530

    implement lazy loading or chunking for About and Grocery components

--- Episode04/src/App.js
+++ Episode04/src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Shimmer from "./components/Shimmer";
+
+const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"));
 
 const App = () => {
   return (
@@ -28,11 +31,23 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <About />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurants/:resId",
---
+++ Episode04/src/components/Grocery.js
@@ -0,0 +1,5 @@
+const Grocery = () => {
+  return <h1>Welcome to Grocery</h1>;
+};
+
+export default Grocery;
