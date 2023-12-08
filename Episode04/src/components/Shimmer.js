e410b72d2f244c884be0d2db85bdb6930f8322b8
Preethi <selvampreethi360@gmail.com>
2023-12-08 14:25:08 +0530

    add new tailwind classes to shimmer UI

--- Episode04/src/components/Shimmer.js
+++ Episode04/src/components/Shimmer.js
@@ -1,14 +1,12 @@
 const Shimmer = () => {
   return (
-    <>
-      <div className="shimmer-container">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-      </div>
-    </>
+    <div className="flex gap-4">
+      <div className="bg-gray-200 w-96 h-96 rounded-lg"></div>
+      <div className="bg-gray-200 w-96 h-96 rounded-lg"></div>
+      <div className="bg-gray-200 w-96 h-96 rounded-lg"></div>
+      <div className="bg-gray-200 w-96 h-96 rounded-lg"></div>
+      <div className="bg-gray-200 w-96 h-96 rounded-lg"></div>
+    </div>
   );
 };
 
