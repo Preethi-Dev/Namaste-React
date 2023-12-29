f03ed209f14ec74ce6a9b41ccc2b51a78a2d0bc0
Preethi <selvampreethi360@gmail.com>
2023-12-29 12:58:06 +0530

    provide store to app

--- Redux-01/src/app.js
+++ Redux-01/src/app.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./components/Body";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <Body />
-    </>
+    </Provider>
   );
 };
 
