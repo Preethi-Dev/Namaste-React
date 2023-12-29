09cdee249ed875aaf715d036f1d160d89a4bb11e
Preethi <selvampreethi360@gmail.com>
2023-12-29 10:38:12 +0530

    create starter files for small redux app

---
+++ Redux-01/src/components/Body.js
@@ -0,0 +1,11 @@
+const Body = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold capitalize text-center">
+        Welcome to redux store
+      </h1>
+    </div>
+  );
+};
+
+export default Body;
